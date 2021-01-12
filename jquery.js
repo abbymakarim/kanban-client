199960425aa4419869946fedc9a1d69f24bc2c08
abbymakarim <abby.makarim@gmail.com>
2021-01-12 21:05:36 +0700

    added feature login and google login

--- jquery.js
+++ jquery.js
@@ -1,13 +1,63 @@
 const baseUrl = "http://localhost:3000"
 
 $(function(){
-    $("#main-page").hide()
-    $("#login-page").show()
+    check()
 })
+
+
+$("#login-btn").on('click', function(event){
+    event.preventDefault()
+    login()
+})
+
+$("#logout-btn").on('click', function(event){
+    event.preventDefault()
+    localStorage.clear()
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+    check()
+})
+
+function check(){
+    if(localStorage.access_token){
+        $("#main-page").show()
+        $("#login-page").hide()
+    } else {
+        $("#main-page").hide()
+        $("#login-page").show()
+    }
+}
+
+function login(){
+    const email = $("#email").val()
+    const password = $("#password").val()
+    $.ajax({
+        method : "POST",
+        url : `${baseUrl}/login`,
+        data : {
+            email,
+            password
+        }
+    })
+    .done(response => {
+        localStorage.setItem('access_token', response.access_token)
+        $("#login-error").empty()
+        check()
+    })
+    .fail(error => {
+        $("#login-error").empty()
+        $("#login-error").append(`<h5 style="color: red;">${error.responseJSON.message}<h5>`)
+    })
+    .always(() => {
+        $("#email").val('')
+        $("#password").val('')
+    })
+}
 
 function onSignIn(googleUser) {
     const id_token = googleUser.getAuthResponse().id_token;
-    console.log(id_token)
     $.ajax({
         method : "POST",
         url : `${baseUrl}/loginGoogle`,
@@ -17,7 +67,7 @@ function onSignIn(googleUser) {
     })
     .done(response => {
         localStorage.setItem('access_token', response.access_token)
-        authorization()
+        check()
     }).fail((xhr, status) => {
 
     })
