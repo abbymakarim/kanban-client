3734a4495b1d1c0dafe708391e51bc8ebb454c7b
abbymakarim <abby.makarim@gmail.com>
2021-01-12 22:36:04 +0700

    add register feature

--- jquery.js
+++ jquery.js
@@ -4,6 +4,10 @@ $(function(){
     check()
 })
 
+$("#register-btn").on('click', function(event){
+    event.preventDefault()
+    register()
+})
 
 $("#login-btn").on('click', function(event){
     event.preventDefault()
@@ -20,14 +24,65 @@ $("#logout-btn").on('click', function(event){
     check()
 })
 
+$("#btn-sign-up").on('click', function(event){
+    event.preventDefault()
+    $("#form-login-button").hide()
+    $("#change-form-signup").hide()
+    $("#form-register-button").show()
+    $("#change-form-login").show()
+})
+
+$("#change-form-login").on('click', function(event){
+    event.preventDefault()
+    $("#form-login-button").show()
+    $("#change-form-signup").show()
+    $("#form-register-button").hide()
+    $("#change-form-login").hide()
+})
+
 function check(){
     if(localStorage.access_token){
         $("#main-page").show()
         $("#login-page").hide()
+        $("#navbar-main").show()
     } else {
         $("#main-page").hide()
         $("#login-page").show()
+        $("#navbar-main").hide()
+        $("#form-register-button").hide()
+        $("#change-form-login").hide()
     }
+}
+
+function register(){
+    const email = $("#email").val()
+    const password = $("#password").val()
+    $.ajax({
+        method : "POST",
+        url : `${baseUrl}/register`,
+        data : {
+            email,
+            password
+        }
+    })
+    .done(response => {
+        $("#login-error").empty()
+        $("#form-login-button").show()
+        $("#change-form-signup").show()
+        check()
+    })
+    .fail(err => {
+        $("#login-error").empty()
+        err.responseJSON.Errors.forEach(el =>{
+            $("#login-error").append(`
+            <h5 style="color: red;">${el}<h5>
+            `)
+        })
+    })
+    .always(() => {
+        $("#email").val('')
+        $("#password").val('')
+    })
 }
 
 function login(){
